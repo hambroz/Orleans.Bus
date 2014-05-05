d2127b72ccb96a43e3f5462b1eeda9c5ec3793ed
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-05-05 19:30:40 +0300

    Bumped version

--- Source/ProductAssemblyInfo.cs
+++ Source/ProductAssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 
 [assembly: AssemblyProduct("Orleans.Bus")]
 [assembly: AssemblyCopyright("Copyright © Yevhen Bobrov 2014")]
